62ab83b16ecfb22bcf0860ec93f9b168804e3fd1
Praveen Kumar Tawari <104024963+babubabubabu123@users.noreply.github.com>
2022-12-11 00:49:28 +0530

    Delete unit6 1.py

--- Python_project/login/unit6 1.py
+++
@@ -1,9 +0,0 @@
-from matplotlib import pyplot as plt
-x=[1,2,3]
-y=[4,5,1]
-plt.bar(x,y,label='LineOnee',linewidth=5)
-plt.title('info')
-plt.ylabel('yaxis')
-plt.xlabel('xaxis')
-plt.legend()
-plt.show()
